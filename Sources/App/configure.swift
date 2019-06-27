dc176e5a5c48ac807af6b809ad14d3d43b38b869
Jussi Suojanen <jussi.suojanen@vincit.fi>
2019-06-27 18:41:43 +0300

    Clean project and add mysql database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,30 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
-/// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
 
-    // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    // Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+
+    var middlewares = MiddlewareConfig()
+    middlewares.use(ErrorMiddleware.self)
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
+    let databaseConfig = MySQLDatabaseConfig(
+        hostname: Environment.get("DB_HOSTNAME")!,
+        username: Environment.get("DB_USER")!,
+        password: Environment.get("DB_PASSWORD")!,
+        database: Environment.get("DB_DATABASE")!
+    )
 
-    // Configure migrations
+    let database = MySQLDatabase(config: databaseConfig)
+    databases.add(database: database, as: .mysql)
+    services.register(database)
+
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 }

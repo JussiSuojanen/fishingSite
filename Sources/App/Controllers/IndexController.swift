cce7aa4b7a83d87d4472cf645cbae949c852bd9c
Jussi Suojanen <jussi.suojanen@vincit.fi>
2019-07-29 11:04:55 +0300

    Add login.leaf and implement necessary routes to website controller

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -1,13 +1,61 @@
 import Vapor
 import Leaf
+import Authentication
 
 struct IndexController: RouteCollection {
 
     func boot(router: Router) throws {
-        router.get(use: indexHandler)
+        let authSessionRoutes =
+            router.grouped(User.authSessionsMiddleware())
+
+        authSessionRoutes.get(use: indexHandler)
+        authSessionRoutes.get("login", use: loginHandler)
+        authSessionRoutes.post(LoginPostData.self, at: "login",
+                    use: loginPostHandler)
     }
 
     func indexHandler(_ req: Request) throws -> Future<View> {
         return try req.view().render("index")
     }
+
+    func loginHandler(_ req: Request) throws -> Future<View> {
+        let context: LoginContext
+
+        if req.query[Bool.self, at: "error"] != nil {
+            context = LoginContext(loginError: true)
+        } else {
+            context = LoginContext()
+        }
+
+        return try req.view().render("login", context)
+    }
+
+    func loginPostHandler(_ req: Request, userData: LoginPostData) throws -> Future<Response> {
+        return User.authenticate(
+            username: userData.username,
+            password: userData.password,
+            using: BCryptDigest(),
+            on: req).map(to: Response.self) { user in
+                guard let user = user else {
+                    return req.redirect(to: "/login?error")
+                }
+                try req.authenticateSession(user)
+
+                return req.redirect(to: "/")
+        }
+    }
+}
+
+struct LoginContext: Encodable {
+    let title = "Log in"
+    let loginError: Bool
+
+    init(loginError: Bool = false) {
+        self.loginError = loginError
+    }
+}
+
+struct LoginPostData: Content {
+    let username: String
+    let password: String
 }

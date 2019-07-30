5816e6cec91d8c1d4a5d2c5c089001a313c493ab
Jussi Suojanen <jussi.suojanen@vincit.fi>
2019-07-30 12:19:43 +0300

    Validate register information

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -64,11 +64,31 @@ struct IndexController: RouteCollection {
     }
 
     func registerHandler(_ req: Request) throws -> Future<View> {
-        let context = RegisterContext()
+        let context: RegisterContext
+        if let message = req.query[String.self, at: "message"] {
+            context = RegisterContext(message: message)
+        } else {
+            context = RegisterContext()
+        }
+
         return try req.view().render("register", context)
     }
 
     func registerPostHandler(_ req: Request, data: RegisterData) throws -> Future<Response> {
+        do {
+            try data.validate()
+        } catch (let error) {
+            let redirect: String
+            if let error = error as? ValidationError,
+                let message = error.reason.addingPercentEncoding(
+                    withAllowedCharacters: .urlQueryAllowed) {
+                redirect = "/register?message=\(message)"
+            } else {
+                redirect = "/register?message=Unknown+error"
+            }
+            return req.future(req.redirect(to: redirect))
+        }
+
         let password = try BCrypt.hash(data.password)
         let user = User(username: data.username, password: password)
 
@@ -102,10 +122,32 @@ struct LoginPostData: Content {
 
 struct RegisterContext: Encodable {
     let title = "Register"
+    let message: String?
+
+    init(message: String? = nil) {
+        self.message = message
+    }
 }
 
 struct RegisterData: Content {
     let username: String
     let password: String
     let confirmPassword: String
+}
+
+extension RegisterData: Validatable, Reflectable {
+    static func validations() throws
+        -> Validations<RegisterData> {
+            var validations = Validations(RegisterData.self)
+            try validations.add(\.username,
+                .alphanumeric && .count(2...))
+            try validations.add(\.password, .count(5...))
+
+            validations.add("passwords match") { model in
+                guard model.password == model.confirmPassword else {
+                    throw BasicValidationError("passwords don’t match")
+                }
+            }
+            return validations
+    }
 }
